e01f5e4dbc699f5249403721125686bddec6abc3
MDProyectos <51146679+MDProyectos@users.noreply.github.com>
2021-01-11 18:21:01 -0400

    Add files via upload

---
+++ Data V6/Menu.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Data_V6
+{
+    public class Menu
+    {
+        
+
+        
+    }
+}
---
+++ Data V6/UserDB.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace Data_V6
+{
+    public class UserDB
+    { 
+        private static User user;
+        private static List<User> users = new List<User>();
+        private static string path;
+        public UserDB (string p)
+        {
+            path = p;
+        }
+        public List<User> GetAllUsers ()
+        {
+            if (File.Exists(path)) 
+            {  
+                string[] lines = File.ReadAllLines(path);  
+                foreach(string line in lines)
+                {
+                    string[] register = line.Split(",");
+                    User u = new User(register);
+                    user = u;
+                    users.Add(user);
+                }
+            }
+            return users;  
+        }
+        public User GetUser (string id)
+        {
+            if (File.Exists(path)) 
+            {  
+                
+                string[] lines = File.ReadAllLines(path);  
+                foreach(string line in lines)
+                {  
+                    if (line.Contains(id))
+                    {
+                        string[] register = line.Split(",");
+                        User u = new User(register);
+                        user = u;
+                        break;
+                    }
+                }
+            }
+            return user;
+        }
+        public void AddUser(User u)
+        {
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(path, true))
+            {
+                file.WriteLine(u.ToFile());
+            }
+        }
+        public void EditUser(User u, string id)
+        {
+            string[] lines = File.ReadAllLines(path); 
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach(var line in lines)
+                {
+                    if (line.Contains(id))
+                    {
+                        writer.WriteLine(u.ToFile());
+                    }
+                    else
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+        public void DeleteUser(string id)
+        {
+            string[] lines = File.ReadAllLines(path); 
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach(var line in lines)
+                {
+                    if (line.Contains(id))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+    }
+}
